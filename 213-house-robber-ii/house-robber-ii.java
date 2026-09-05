1fc7a58d4a70334d667002fa6f5c5b9ef057c4f8
Devskg04 <devgyanchandani22@gmail.com>
2026-09-05 13:21:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (22.77%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -11,7 +11,6 @@ class Solution {
         return Math.max(helper(0,n-2,nums),helper(1,n-1,nums));
     }
     public int helper(int st,int end,int[] nums){
-        int[] a=new int[nums.length-1];
         int prev2=nums[st];
         int prev1=Math.max(nums[st],nums[st+1]);
         int current=prev1;
